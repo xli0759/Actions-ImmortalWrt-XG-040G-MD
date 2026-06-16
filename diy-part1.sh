ce6c7ba0d211df65aab425b2bc5882a86912936d
xli0759 <xli0759@gmail.com>
2026-06-16 17:23:38 +0800

    Update diy-part1.sh
    尝试编译稳定版25.12

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,4 +11,7 @@
 #
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i '/an7581-evb-emmc-eagle/d' target/linux/airoha/image/Makefile
+sed -i '/define Device\/airoha_an7581-evb-emmc-eagle/,/^endef/d' target/linux/airoha/image/an7581.mk
+sed -i '/TARGET_DEVICES += airoha_an7581-evb-emmc-eagle/d' target/linux/airoha/image/an7581.mk
+sed -i '/define Device\/airoha_an7581-evb-emmc-kite/,/^endef/d' target/linux/airoha/image/an7581.mk
+sed -i '/TARGET_DEVICES += airoha_an7581-evb-emmc-kite/d' target/linux/airoha/image/an7581.mk
