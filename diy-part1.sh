c18fabbf827d42626d79c99cd6648110c104b346
xli0759 <xli0759@gmail.com>
2026-06-16 21:25:20 +0800

    Update diy-part1.sh
    更新target/linux/airoha/image/an7581.mk，尝试编译25.12

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,39 @@
 #
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i '/define Device\/airoha_an7581-evb-emmc-eagle/,/^endef/d' target/linux/airoha/image/an7581.mk
-sed -i '/TARGET_DEVICES += airoha_an7581-evb-emmc-eagle/d' target/linux/airoha/image/an7581.mk
-sed -i '/define Device\/airoha_an7581-evb-emmc-kite/,/^endef/d' target/linux/airoha/image/an7581.mk
-sed -i '/TARGET_DEVICES += airoha_an7581-evb-emmc-kite/d' target/linux/airoha/image/an7581.mk
+cat > target/linux/airoha/image/an7581.mk << 'EOF'
+define Build/an7581-preloader
+  cat $(STAGING_DIR_IMAGE)/an7581_$1-bl2.fip >> $@
+endef
+
+define Build/an7581-bl31-uboot
+  cat $(STAGING_DIR_IMAGE)/an7581_$1-bl31-u-boot.fip >> $@
+endef
+
+define Device/FitImageLzma
+	KERNEL_SUFFIX := -uImage.itb
+	KERNEL = kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(DEVICE_DTS).dtb
+	KERNEL_NAME := Image
+endef
+
+define Device/bell_xg-040g-md
+  $(call Device/FitImageLzma)
+  DEVICE_VENDOR := Nokia
+  DEVICE_MODEL := Bell XG-040G-MD
+  DEVICE_VARIANT := Router Mode
+  DEVICE_DTS := an7581-bell_xg-040g-md
+  SOC := an7581
+  KERNEL_LOADADDR := 0x80088000
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_SIZE := 8192k
+  IMAGE_SIZE := 261120k
+  KERNEL_IN_UBI := 1
+  UBINIZE_OPTS := -s 2048
+  IMAGES := factory.bin sysupgrade.bin
+  IMAGE/factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  DEVICE_PACKAGES := kmod-phy-airoha-en8811h kmod-i2c-an7581 kmod-leds-gpio kmod-gpio-button-hotplug uboot-envtools ubi-utils kmod-usb-storage-uas blkid lsblk
+endef
+TARGET_DEVICES += bell_xg-040g-md
+EOF
